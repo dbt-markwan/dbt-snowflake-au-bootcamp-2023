25451cbcf45a6dd24b181155f041b0be78d7aa1b
Jay Kim <jay.kim@sky.co.nz>
2022-12-14 01:44:07 +0000

    Exercise 2

---
+++ models/marts/dim_players.sql
@@ -0,0 +1,12 @@
+with player as (
+select * from {{ ref('stg_player') }}
+),
+team as (
+select * from {{ ref('stg_team') }}
+)
+select
+player.*
+, team.team_name
+from player
+left join team
+on player.affiliation_id = team.affiliation_id
