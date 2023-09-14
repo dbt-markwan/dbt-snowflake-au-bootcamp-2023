52885025f4ea27419cdb681f7d77c2049f47af0e
Daniel Raja Manohar <danny.rajamanohar@qrious.co.nz>
2023-09-14 02:41:51 +0000

    concat player name and got the player age

--- models/staging/fifa/stg_fifa__player.sql
+++ models/staging/fifa/stg_fifa__player.sql
@@ -9,11 +9,13 @@ source as (
 renamed as (
 
     select
-        id,
+        id as player_id,
         player_first_name,
         player_middle_name,
         player_last_name,
         player_known_name,
+        concat(player_first_name, ' ',player_last_name) as player_name,
+        datediff(year, birth_date, '2018-06-14') as age,
         birth_date,
         weight,
         height,
