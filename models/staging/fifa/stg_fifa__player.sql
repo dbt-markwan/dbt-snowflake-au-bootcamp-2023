3dbfa579e68e2e6caa817d25fc3de004921ebffd
Daniel Raja Manohar <danny.rajamanohar@qrious.co.nz>
2023-09-14 02:34:24 +0000

    Created Staging model for player

---
+++ models/staging/fifa/stg_fifa__player.sql
@@ -0,0 +1,28 @@
+with 
+
+source as (
+
+    select * from {{ source('fifa', 'player') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        player_first_name,
+        player_middle_name,
+        player_last_name,
+        player_known_name,
+        birth_date,
+        weight,
+        height,
+        city,
+        national_team_affiliation_id,
+        affiliation_id
+
+    from source
+
+)
+
+select * from renamed
